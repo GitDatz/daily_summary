b52b6da4c1f94e28977869910be916c6dbcadf0b
VD Thai <vidat.thai@gmail.com>
2022-04-14 21:22:52 +0200

    Add and remove tasks

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include <ftxui/dom/elements.hpp>
-#include <ftxui/dom/table.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
@@ -22,6 +22,9 @@ int main() {
     using namespace ftxui;
 
     std::string task_str;
+    std::vector<std::string> task_vector;
+    int selected_task = 0;
+
     auto screen = ScreenInteractive::TerminalOutput();
  
     Component task_input = Input(&task_str, "Task");
@@ -30,24 +33,29 @@ int main() {
         return vbox({
                 text(" Daily Summary") | bold,
                 text(get_current_date_string()),
-                separator(),
+                separatorLight(),
             });
     });
 
     auto input_component = Renderer(task_input, [&] {
-        return hbox(text(" Task : "), task_input->Render());
+        return hbox(text(" Task : "), task_input->Render()) | border;
     });
 
     auto buttons_container = Container::Horizontal({
-        Button("Add Task", [&] {}),
-        Button("Quit", screen.ExitLoopClosure()),
+        Button(" Add Task ", [&] { task_vector.push_back(task_str); }, ButtonOption::Simple()),
+        Button(" Quit ", screen.ExitLoopClosure(), ButtonOption::Simple()),
     });
+
+    MenuOption option;
+    option.on_enter = [&] { task_vector.erase(task_vector.begin() + selected_task); };
+    auto menu = Menu(&task_vector, &selected_task, &option) | border;
 
     screen.Loop(Container::Vertical({
         header_component,
+        menu,
         input_component,
         buttons_container
-    }) | border);
+    }) | borderDouble );
     
     return 0;
 }
