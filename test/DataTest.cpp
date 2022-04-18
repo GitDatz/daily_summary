9d424a4ed938fdb5d59bb374d8e2c7cf66219806
VD Thai <vidat.thai@gmail.com>
2022-04-18 10:28:27 +0200

    Unit tests for Data class

--- test/DataTest.cpp
+++ test/DataTest.cpp
@@ -1,11 +1,53 @@
 #include "../src/model/Data.hpp"
 #include <gtest/gtest.h>
 
-TEST(DataTests, DummyDataTest)
+#include <functional>
+#include <string>
+
+class DataTest: public ::testing::Test {
+protected:
+    void SetUp() override {
+        mData = std::make_unique<model::Data>(mDateString);
+    }
+
+    void TearDown() override {
+        mData.reset();
+    }
+    const std::string mDateString = "2022-02-22";
+    std::unique_ptr<model::Data> mData;
+};
+
+TEST_F(DataTest, DateStringTest)
 {
-    const auto expected = 1;
-    const auto actual = 1;
-    ASSERT_EQ(expected, actual);
+    const std::string EXPECTED_DATE_STRING = "2022-02-22";
+    ASSERT_EQ(EXPECTED_DATE_STRING, mData->GetDateString());
+}
+
+TEST_F(DataTest, AddTaskTest)
+{
+    const int EXPECTED_NUMBER_OF_TASKS = 5;
+    for (int i = 0; i < EXPECTED_NUMBER_OF_TASKS; i++) {
+        const std::string task("Task " + std::to_string(i));
+        mData->AddTask(task);
+    }
+    ASSERT_EQ(mData->GetTaskVector().size(), EXPECTED_NUMBER_OF_TASKS);
+}
+
+TEST_F(DataTest, DeleteTaskTest)
+{
+    const int INITIAL_NUMBER_OF_TASKS = 5;
+    for (int i = 0; i < INITIAL_NUMBER_OF_TASKS; i++) {
+        const std::string task("Task " + std::to_string(i));
+        mData->AddTask(task);
+    }
+    ASSERT_EQ(mData->GetTaskVector().size(), INITIAL_NUMBER_OF_TASKS);
+    const int NUMBER_OF_DELETED_TASKS = 3;
+    for (int i = 0; i < NUMBER_OF_DELETED_TASKS; i++) {
+        mData->DeleteTask(i);
+    }
+    const int FINAL_NUMBER_OF_TASKS =
+        INITIAL_NUMBER_OF_TASKS - NUMBER_OF_DELETED_TASKS;
+    ASSERT_EQ(mData->GetTaskVector().size(), FINAL_NUMBER_OF_TASKS);
 }
 
 int main(int argc, char** argv)
